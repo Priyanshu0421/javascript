140e358069ae6eb9a020c95abe58afd134c3ffde
Priyanshu0421 <103637734+Priyanshu0421@users.noreply.github.com>
2024-06-23 12:04:34 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all Js code as newer version
+
+console.log(3+3)
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+// DataTypes
+// number => 2 to pwer of 53
+// bigint => for number bigger than 2 to power of 53
+// String => " "
+// null => Standout Value -> Speacial value
+// undefined => value is not yet defined
+// Symbol => Unique
+
+
+console.log(typeof age) // number
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
