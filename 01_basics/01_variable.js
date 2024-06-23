12930b10b8d2934a85ea39b5533f558139e4abe2
Priyanshu0421 <103637734+Priyanshu0421@users.noreply.github.com>
2024-06-23 13:54:24 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144453
+let accountEmail = "priyanshu@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+// console.table([accountId,accountEmail,accountPassword,accountCity])
+
+accountEmail = "priyanshu01@gmail.com"
+accountPassword = "2121"
+accountCity = "Bengluru"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
