67aaf3bba5c9d32782b9dfe4296faf56df0fab46
Priyanshu0421 <103637734+Priyanshu0421@users.noreply.github.com>
2024-07-06 03:35:34 +0000

    controlFlow basics

---
+++ 04_control_flow/one.js
@@ -0,0 +1,36 @@
+const isLoggedIn = true;
+
+const temp = 41
+
+// if(isLoggedIn){
+//     console.log("you are doing , fine");
+// }else{
+//     console.log("you are not doing fine");
+// }
+
+// if(temp < 50){
+//     console.log('temp is less than 50');
+// }else{
+//     console.log('temp is greator than 50');
+// }
+
+
+const balance = 1000;
+
+// if(balance > 500) console.log("test") , console.log('test2');  -> not a good practice 
+
+
+const userLoggedIn = true;
+const debitCardDetails = true
+
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+// if(userLoggedIn && debitCardDetails){
+//     console.log('You can Buy the Products');
+// }
+
+// if(loggedInFromEmail || loggedInFromGoogle){
+//     console.log('You are Already Logged In');
+// }
